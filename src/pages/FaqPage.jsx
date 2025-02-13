fc4198a3abc886ee9aa57b54006a10db8a89b246
jondumad <anferneehhh@gmail.com>
2025-02-13 14:44:06 +0700

    FAQPage

--- src/pages/FaqPage.jsx
+++ src/pages/FaqPage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import FaqComponent from "../components/FaqComponent";
+import { Container, Row, Col } from "react-bootstrap";
 
 const FaqPage = () => {
-	return <div>FaqPage</div>;
+	return (
+		<div className="faq py-5">
+			<Container>
+				<Row>
+					<Col>
+						<h2 className="text-center fw-bold my-5">FAQ</h2>
+					</Col>
+				</Row>
+				<Row className="row-cols-lg-2 g-2 pt-5">
+					<FaqComponent />
+				</Row>
+			</Container>
+		</div>
+	);
 };
 
 export default FaqPage;
