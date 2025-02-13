5c19050f1069db1b07745d774acde5f8c8a77951
jondumad <anferneehhh@gmail.com>
2025-02-13 12:22:27 +0700

    NavBar, Homepage(Header dan Kelas)

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { kelasTerbaru } from "../data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { Container, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import HeroImage from "../assets/img/hero.png"; // Import hero image
+
+const HomePage = () => {
+	const navigate = useNavigate();
+
+	return (
+		<div className="homepage">
+			<header className="w-100 min-vh-100 d-flex align-items-center">
+				<Container>
+					<Row className="header-box d-flex align-items-center">
+						<Col lg="6">
+							<h1 className="mb-4">
+								Temukan <br /> <span>Bakat Kreatifmu</span> Bersama Kami!
+							</h1>
+							<p className="mb-4">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Veritatis expedita doloribus est porro consequuntur impedit.
+							</p>
+							<button
+								className="btn btn-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2"
+								onClick={() => navigate("/kelas")}
+							>
+								Lihat Kelas
+							</button>
+							<button className="btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2">
+								Lihat Promo
+							</button>
+						</Col>
+						<Col lg="6" className="pt-lg-0 pt-5">
+							<img src={HeroImage} alt="hero-img" className="img-fluid" />
+						</Col>
+					</Row>
+				</Container>
+			</header>
+
+			<div className="kelas w-100 min-vh-100 py-5">
+				<Container>
+					<Row className="row">
+						{kelasTerbaru.map((kelas) => (
+							<Col key={kelas.id} lg={4} md={6} sm={12} className="mb-4">
+								<img
+									src={kelas.image}
+									alt={kelas.title}
+									className="w-100 mb-3 rounded-top img-fluid img-kelas"
+								/>
+								<div className="star mb-2">
+									{[...Array(5)].map((_, i) => (
+										<FontAwesomeIcon
+											key={i}
+											icon={faStar}
+											className={kelas.star1 > i ? "active" : ""}
+										/>
+									))}
+								</div>
+								<h5 className="mb-3">{kelas.title}</h5>
+								<div className="ket d-flex justify-content-between align-items-center">
+									<p className="m-0 text-primary fw-bold">{kelas.price}</p>
+									<button className="btn btn-danger rounded-1">
+										{kelas.buy}
+									</button>
+								</div>
+							</Col>
+						))}
+					</Row>
+					<Row>
+						<Col className="text-center mt-4">
+							<button
+								className="btn btn-success rounded-5 btn-lg"
+								onClick={() => navigate("/kelas")}
+							>
+								Lihat Semua Kelas{" "}
+								<FontAwesomeIcon icon={faChevronRight} className="ms-1" />
+							</button>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
---
+++ src/pages/SyaratKetentuanPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SyaratKetentuanPage = () => {
+	return <div>SyaratKetentuanPage</div>;
+};
+
+export default SyaratKetentuanPage;
