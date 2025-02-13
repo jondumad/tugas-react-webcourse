d4477ef0bb984b928f2578f10efe75a5a1a9433e
jondumad <anferneehhh@gmail.com>
2025-02-13 20:15:23 +0700

    Scroll To Top

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"; // Imp
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./dist/css/main.css";
+import ScrollToTop from "./components/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<ScrollToTop />
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
